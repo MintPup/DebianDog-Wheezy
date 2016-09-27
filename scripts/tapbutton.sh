8e2d1baa5d76a6744614b4cdccaea87043220848
MintPup <saintless@abv.bg>
2016-09-27 20:06:40 +0300

    Create tapbutton.sh

---
+++ scripts/tapbutton.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# GNU GPL v3 applies. No warranty of any kind... Use it at your own risk!
+
+synclient TapButton1=1
+synclient TapButton2=2
+synclient TapButton3=3
+synclient VertEdgeScroll=1
